ee0609c2215e09f7cc5748a46d2fde8374a91e34
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2024-05-04 17:52:39 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".btn").addEventListener("click", function() {
+    generateCard();
+  });
 };
+function generateCard() {
+  let suits = ["♠", "♥", "♦", "♣"];
+  let rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  let randomRank = rank[Math.floor(Math.random() * rank.length)];
+  if (randomSuit === "♠" || randomSuit === "♣") {
+    document.querySelector(".top").style.color = "black";
+    document.querySelector(".bottom").style.color = "black";
+    document.querySelector(".rank").style.color = "black";
+  } else {
+    document.querySelector(".top").style.color = "red";
+    document.querySelector(".bottom").style.color = "red";
+    document.querySelector(".rank").style.color = "red";
+  }
+  document.querySelector(".top").innerHTML = randomSuit;
+  document.querySelector(".bottom").innerHTML = randomSuit;
+  document.querySelector(".rank").innerHTML = randomRank;
+}
